d326bd208ad87d2a04069647deb1e0fc2895f9d0
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-13 16:23:43 +0530

    header component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
