096a4985168467c1b41fe1c82aafeae38a2b3e06
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-04-19 11:23:03 +0530

    create event component created

--- src/app/components/create-event/create-event.component.ts
+++ src/app/components/create-event/create-event.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Event } from '../../models/event';
+
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styleUrls: ['./create-event.component.css']
 })
 export class CreateEventComponent implements OnInit {
+  eventList: Event[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  model = new Event(1, 'event one', '2020-04-20', '16:00');
+
+  onSubmit() {
+    let newEvent: Event = new Event(Date.now(), this.model.name, this.model.date, this.model.time);
+
+    this.eventList.push(newEvent);
+
+    console.log(this.eventList)
+  }
 }
